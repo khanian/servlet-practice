87ee0d146780be836cc1b9ca94cb9a5754cf4345
khanian <khanylee@gmail.com>
2023-03-04 08:17:54 +0900

    add first

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -1,37 +1,29 @@
 package org.example;
 
+import org.example.calculator.Calculator;
+import org.example.calculator.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet implements Servlet {
+public class CalculatorServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(CalculatorServlet.class);
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        logger.info("init");
-    }
-
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         logger.info("service");
-    }
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
 
-    @Override
-    public void destroy() {
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 }
